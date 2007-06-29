9948452128b60a82c96fbc3b9de5b38d8f0165f2
ramzi.benyahia <ramzi.benyahia@112126f8-0734-0410-bb18-b3d48ac8bcd4>
2007-06-29 10:02:18 +0000

    not really convinced but listening to messages works

---
+++ Stomp.hs
@@ -0,0 +1,101 @@
+module Stomp (
+  connect ,        
+  disconnect,
+  send,
+  subscribe,
+  Listener (..)
+)
+
+where
+
+import Network
+import IO
+import Control.Monad.State
+import Control.Monad
+import Control.Concurrent
+import System
+
+data Command = CONNECT | DISCONNECT | SEND | 
+               SUBSCRIBE | UNSUBSCRIBE | ACK
+               deriving (Show,Eq)
+
+data Client = Client Handle
+
+type Headers = [(String,String)]
+type Destination = String
+
+newtype Listener a = Listener { 
+      onMessage :: Headers -> String -> IO a
+    }
+
+
+splitToPair :: String -> Char -> (String,String)
+splitToPair str sep = 
+    (takeWhile (/= sep) str, tail $ dropWhile (/= sep) str)
+
+parseHeaders :: Handle -> StateT Headers IO Headers
+parseHeaders h = do 
+  headers <- get
+  line <- lift $ hGetLine h
+  if line == "" then return headers
+                else do 
+                  modify ((splitToPair line ':') : )
+                  parseHeaders h
+
+parseBody :: Handle -> StateT String IO String
+parseBody h = do
+  body <- get
+  c <- lift $ hGetChar h
+  if c == '\NUL' then return $ reverse body
+                 else do 
+                   modify (c:)
+                   parseBody h
+
+parse :: Handle -> IO (String,Headers,String)
+parse h = do
+  command  <- hGetLine h
+  if command == "" then parse h
+       else do
+         headers  <- evalStateT (parseHeaders h) []
+         body     <- evalStateT (parseBody h) ""
+         return (command,headers,body)
+
+connect :: HostName -> PortNumber -> Headers -> IO Client
+connect host port headers = withSocketsDo $ do
+  h <- connectTo host  (PortNumber port)
+  hSetBuffering h NoBuffering
+  netSend h CONNECT headers ""
+  resp <- parse h
+  print "connected .."
+  return $ Client h
+
+disconnect :: Client -> Headers -> IO ()
+disconnect (Client h) hdrs = netSend h DISCONNECT hdrs "" >> hClose h
+
+send :: Client -> Destination -> Headers -> String -> IO ()
+send (Client h) dest headers = netSend h SEND (("destination",dest):headers)
+    
+headersStr :: Headers -> String
+headersStr  = concat . map (\(x,y) -> concat [x,": ",y,"\n"])
+    
+netSend :: Handle -> Command -> Headers -> String -> IO ()
+netSend h cmd hdrs body = do
+  let hdrs' = ("content-length",show $ length body):hdrs
+  let str = concat [show cmd,"\n",headersStr hdrs',"\n",body,"\n\NUL"]
+  hPutStr h str
+  hFlush h
+  putStrLn "data sent .. : "
+  putStrLn str
+
+subscribe :: Client -> Destination -> Headers -> Listener a -> IO ()    
+subscribe (Client h) dest headers listener = do
+  netSend h SUBSCRIBE  (("destination",dest):headers)  ""
+  print $ "subscribed to " ++ dest ++ " .."
+  listenWith h listener
+  
+  
+listenWith :: Handle -> Listener a -> IO ()                 
+listenWith h listener = do 
+  (cmd,headers,body) <- parse h
+  (onMessage listener) headers body
+  listenWith h listener
---
+++ main.hs
@@ -0,0 +1,11 @@
+import Stomp
+import System
+
+main = do 
+  args <- getArgs
+  client<- connect "localhost" 61613 []
+  subscribe client (args !! 0) [] $ 
+            Listener  (\hdrs msg -> do
+                         print (hdrs,msg)
+                         send client "/queue/bar" [] ("resp:" ++ msg)
+                      )
