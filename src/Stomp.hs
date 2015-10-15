1d05d0f4e91db08718fec53b8e01d049ae4e2c50
Alex Collins <alextcollins@gmail.com>
2015-10-15 19:48:50 +0100

    Format file

--- src/Stomp.hs
+++ src/Stomp.hs
@@ -1,10 +1,10 @@
 module Stomp (
-  connect ,        
-  disconnect,
-  send,
-  subscribe,
-  withSubscriptionDo,
-  subscription,
+  connect ,         
+  disconnect, 
+  send, 
+  subscribe, 
+  withSubscriptionDo, 
+  subscription, 
   Listener (..)
 )
 
@@ -18,11 +18,11 @@ import Control.Concurrent
 
 data Command = CONNECT | DISCONNECT | SEND | 
                SUBSCRIBE | UNSUBSCRIBE | ACK
-               deriving (Show,Eq)
+               deriving (Show, Eq)
 
 data Client = Client Handle
 
-type Headers = [(String,String)]
+type Headers = [(String, String)]
 type Destination = String
 
 newtype Listener a = Listener { 
@@ -30,9 +30,9 @@ newtype Listener a = Listener {
     }
 
 
-splitToPair :: String -> Char -> (String,String)
+splitToPair :: String -> Char -> (String, String)
 splitToPair str sep = 
-    (takeWhile (/= sep) str, tail $ dropWhile (/= sep) str)
+    (takeWhile (/= sep) str,  tail $ dropWhile (/= sep) str)
 
 parseHeaders :: Handle -> StateT Headers IO Headers
 parseHeaders h = do 
@@ -52,21 +52,21 @@ parseBody h = do
                    modify (c:)
                    parseBody h
 
-parse :: Handle -> IO (String,Headers,String)
+parse :: Handle -> IO (String, Headers, String)
 parse h = do
   command  <- hGetLine h
   if command == "" then parse h
        else do
          headers  <- evalStateT (parseHeaders h) []
          body     <- evalStateT (parseBody h) ""
-         return (command,headers,body)
+         return (command, headers, body)
 
 connect :: HostName -> PortNumber -> Headers -> IO Client
 connect host port headers = withSocketsDo $ do
   h <- connectTo host  (PortNumber port)
   hSetBuffering h NoBuffering
   netSend h CONNECT headers ""
-  (cmd,_,_) <- parse h
+  (cmd, _, _) <- parse h
   if cmd == "CONNECTED" then return $ Client h
                         else fail "connection failed" 
 
@@ -74,27 +74,26 @@ disconnect :: Client -> Headers -> IO ()
 disconnect (Client h) hdrs = netSend h DISCONNECT hdrs "" >> hClose h
 
 send :: Client -> Destination -> Headers -> String -> IO ()
-send (Client h) dest headers = netSend h SEND (("destination",dest):headers)
+send (Client h) dest headers = netSend h SEND (("destination",  dest) : headers)
     
 headersStr :: Headers -> String
-headersStr  = concat . map (\(x,y) -> concat [x,": ",y,"\n"])
+headersStr  = concat . map (\(x, y) -> concat [x, ": ", y, "\n"])
     
 netSend :: Handle -> Command -> Headers -> String -> IO ()
 netSend h cmd hdrs body = do
-  let hdrs' = ("content-length",show $  length body):hdrs
-  let str = concat [show cmd,"\n",headersStr hdrs',"\n",body,"\000"]
+  let hdrs' = ("content-length", show $  length body):hdrs
+  let str = concat [show cmd, "\n", headersStr hdrs', "\n", body, "\000"]
   hPutStr h str
   hFlush h
 
-
 subscribe :: Client -> Destination -> Headers -> IO ()
 subscribe (Client h) dest headers  = do
-  netSend h SUBSCRIBE  (("destination",dest):headers)  ""
+  netSend h SUBSCRIBE  (("destination", dest):headers)  ""
 
-subscription :: Client -> IO (Headers,String)
+subscription :: Client -> IO (Headers, String)
 subscription client@(Client h) = do
-  (_,headers,body) <- parse h
-  return (headers,body)
+  (_, headers, body) <- parse h
+  return (headers, body)
 
 withSubscriptionDo :: IO () -> IO ()
 withSubscriptionDo  io = io >>  withSubscriptionDo io
